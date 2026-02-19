cd9de2e1ca94517a34e4658c3bc75eb7832757ba
anyway <zorix228337@gmail.com>
2026-02-19 15:43:04 +0300

    add: weapons heating

---
+++ Project/wo_ow/Assets/Source/Timer.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public bool DoWhile = false;
+    public event Action Action;
+    public float Duration { get; private set; }
+    public float Current { get; private set; }
+
+    public void Set(float time) => Duration = time;
+    public void Run() => Current = Duration;
+
+
+    private void Update()
+    {
+        if (Current <= 0)
+        {
+            Action?.Invoke();
+        }
+        else
+        {
+            Current -= Time.deltaTime;
+
+            if (DoWhile)
+            {
+                Action?.Invoke();
+            }
+        }
+    }
+}
