450aa18a2c6e3e9ca91f28a3f9aba6fbab7f9024
anyway <zorix228337@gmail.com>
2026-02-22 15:01:07 +0300

    add: base script for a neon soldiers

---
+++ Project/wo_ow/Assets/Source/Enemy/NeonSoldier/NeonSoldier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class NeonSoldier : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemy;
+
+    private void Start()
+    {
+        _enemy.Stats = DeserializeData.Deserialize<EnemyStats>("./Assets/Source/Data/NeonSoldierData.json");
+    }
+
+    private void Update()
+    {
+        _enemy.EnemyControls.MoveToTarget(_enemy.Stats.MoveSpeed);
+    }
+}
