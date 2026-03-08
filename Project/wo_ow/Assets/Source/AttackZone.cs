e1a6cf6e913e16214d51a8c582f034f8a60cd101
anyway <zorix228337@gmail.com>
2026-03-08 21:51:41 +0300

    add: script for attack range zone

---
+++ Project/wo_ow/Assets/Source/AttackZone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackZone : MonoBehaviour {
+    public bool SomeoneInAttackRange { get; private set; }
+    public List<GameObject> ObjectsInZone { get; private set; }
+
+    
+    private void OnTriggerEnter(Collider other) {
+        if (!SomeoneInAttackRange)
+            SomeoneInAttackRange = true;
+        
+        ObjectsInZone.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        ObjectsInZone.Remove(other.gameObject);
+
+        if (ObjectsInZone.Count == 0)
+            SomeoneInAttackRange = false;
+    }
+}
