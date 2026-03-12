affb434250bb6ab29cb4021a687c2d96bae46650
anyway <zorix228337@gmail.com>
2026-03-12 20:40:12 +0300

    add: three new methods for enemy controlling

--- Project/wo_ow/Assets/Source/Enemy/EnemyControls.cs
+++ Project/wo_ow/Assets/Source/Enemy/EnemyControls.cs
@@ -12,4 +12,25 @@ public class EnemyControls : MonoBehaviour
             speed * Time.deltaTime
         );
     }
+
+    public void MoveAwayFromTarget(float speed) {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            new Vector3(
+                _target.position.x,
+                0,
+                _target.position.z
+            ),
+            speed * Time.deltaTime
+        );
+    }
+
+//  TODO:
+    public void TeleportBehindTarget() {
+        
+    }
+
+    public void TeleportInFrontOfTarget() {
+        
+    }
 }
