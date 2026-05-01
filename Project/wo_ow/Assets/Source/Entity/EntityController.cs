b3fa08137c4389d66c43375f0ca8a2b9344947d8
anyway <zorix228337@gmail.com>
2026-05-01 21:28:41 +0300

    fix some bugs

--- Project/wo_ow/Assets/Source/Entity/EntityController.cs
+++ Project/wo_ow/Assets/Source/Entity/EntityController.cs
@@ -1,8 +1,30 @@
 ﻿using UnityEngine;
 
-public class EntityController : MonoBehaviour {
-    public void MoveEntity(Vector3 direction, float moveSpeed)
-        => transform.Translate(direction * moveSpeed * Time.deltaTime, Space.Self);
+public class EntityController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+
+    public void MoveEntity(Vector3 direction, float moveSpeed) {
+        direction.Normalize();
+        
+        var cameraForward = Camera.main.transform.forward;
+        var cameraRight = Camera.main.transform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        var moveDirection = cameraForward * direction.z + cameraRight * direction.x;
+        moveDirection.Normalize();
+
+        var velocity = moveDirection * moveSpeed;
+
+        velocity.y = rigidbody.velocity.y;
+
+        rigidbody.velocity = velocity;
+    }
 
     public void RotateEntity(Quaternion rotation)
         => transform.rotation = rotation;
